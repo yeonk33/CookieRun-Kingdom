be8232619b9044fbfeb9ff890d27a39a60c32a48
이연경 <55737485+yeonk33@users.noreply.github.com>
2025-06-26 18:20:46 +0900

    카메라 프리팹 생성, 줌 기능 추가

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,33 +1,64 @@
-﻿using UnityEngine;
+﻿using Cinemachine;
+using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
+    #region Define
+    const string DragAction = "Drag";
+    const string ZoomAction = "Zoom";
+    #endregion
+
+    #region Inspector
     [SerializeField] private InputActionAsset _inputSystem;
-    InputAction _action;
+    private InputAction _drag;
+    private InputAction _zoom;
+
     [SerializeField] private float _sensitivity = 5f;
     private Vector2 _delta;
+    private float _scrollY;
+    
+    private CinemachineVirtualCamera _camera;
+
+    #endregion
 
     private void OnEnable()
     {
-        _action = _inputSystem["drag"];
-        _action.Enable();
-        _action.performed -= OnInputPerformed;
-        _action.performed += OnInputPerformed;
+        _camera = GetComponent<CinemachineVirtualCamera>();
+
+        _drag = _inputSystem[DragAction];
+        _drag.performed -= OnDragPerformed;
+        _drag.performed += OnDragPerformed;
+
+        _zoom = _inputSystem[ZoomAction];
+        _zoom.performed -= OnZoomPerformed;
+        _zoom.performed += OnZoomPerformed;
     }
 
     private void OnDisable()
     {
-        _action.Disable();
-        _action.performed -= OnInputPerformed;
+        _drag.performed -= OnDragPerformed;
+        _zoom.performed -= OnZoomPerformed;
     }
 
-    private void OnInputPerformed(InputAction.CallbackContext context)
+    private void OnDragPerformed(InputAction.CallbackContext context)
     {
         _delta = context.ReadValue<Vector2>();
         if (_delta != Vector2.zero)
         {
             transform.position += new Vector3(-_delta.x, -_delta.y, 0) * Time.deltaTime * _sensitivity;
+        }
+    }
+
+    private void OnZoomPerformed(InputAction.CallbackContext context)
+    {
+        _scrollY = context.ReadValue<float>();
+        
+        if (_scrollY != 0f)
+        {
+            var zoom = _camera.m_Lens.OrthographicSize + _scrollY * 0.02f;
+            _camera.m_Lens.OrthographicSize = Mathf.Clamp(zoom, 1f, 10f);
         }
     }
 }
