e432b406094a49d5e5dad2db2598971da50d11ff
이연경 <55737485+yeonk33@users.noreply.github.com>
2025-06-26 17:47:13 +0900

    Camera Controller 추가

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset _inputSystem;
+    InputAction _action;
+    [SerializeField] private float _sensitivity = 5f;
+    private Vector2 _delta;
+
+    private void OnEnable()
+    {
+        _action = _inputSystem["drag"];
+        _action.Enable();
+        _action.performed -= OnInputPerformed;
+        _action.performed += OnInputPerformed;
+    }
+
+    private void OnDisable()
+    {
+        _action.Disable();
+        _action.performed -= OnInputPerformed;
+    }
+
+    private void OnInputPerformed(InputAction.CallbackContext context)
+    {
+        _delta = context.ReadValue<Vector2>();
+        if (_delta != Vector2.zero)
+        {
+            transform.position += new Vector3(-_delta.x, -_delta.y, 0) * Time.deltaTime * _sensitivity;
+        }
+    }
+}
